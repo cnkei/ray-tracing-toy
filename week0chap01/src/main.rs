5a4b0f2872801ddad20d51712d1f55986f25f4c0
Jason King <chn.kei@gmail.com>
2019-07-04 08:53:42 +0800

    add week0 chap1-4

--- week0chap01/src/main.rs
+++ week0chap01/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+	let nx = 200;
+	let ny = 100;
+	println!("P3\n{} {}\n255", nx, ny);
+	for j in (0..ny).rev() {
+		for i in 0..nx {
+			let r = i as f32 / nx as f32;
+			let g = j as f32 / ny as f32;
+			let b = 0.2;
+			let r = (255.99 * r) as u8;
+			let g = (255.99 * g) as u8;
+			let b = (255.99 * b) as u8;
+			println!("{} {} {}", r, g, b);
+		}
+	}
 }
---
+++ week0chap04/src/main.rs
@@ -0,0 +1,40 @@
+use ray_tracing_toy::{
+    vec3::Vec3, ray::Ray
+};
+
+fn hit_sphere(center: Vec3, radius: f32, r: Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = Vec3::dot(r.direction(), r.direction());
+    let b = 2.0 * Vec3::dot(oc, r.direction());
+    let c = Vec3::dot(oc, oc) - radius * radius;
+    b * b - 4.0 * a * c > 0.0
+}
+
+fn color(r: Ray) -> Vec3 {
+    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
+        Vec3::new(1.0, 0.0, 0.0)
+    } else {
+        let unit_direction = r.direction().unit_vector();
+        let t = 0.5 * (unit_direction.y() + 1.0);
+        (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+    }
+}
+
+fn main() {
+	let nx = 200;
+	let ny = 100;
+	println!("P3\n{} {}\n255", nx, ny);
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::zero();
+	for j in (0..ny).rev() {
+		for i in 0..nx {
+            let u = i as f32 / nx as f32;
+            let v = j as f32 / ny as f32;
+            let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
+			let v = 255.99 * color(r);
+			println!("{} {} {}", v[0] as u8, v[1] as u8, v[2] as u8);
+		}
+	}
+}
